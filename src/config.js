55fc2c4cb8b3e259f8e0a7faa5b635cfd352fafe
aakashs007 <aakashs209@gmail.com>
2026-08-15 17:53:53 +0530

    fixup! done

--- src/config.js
+++ src/config.js
@@ -1,14 +1,12 @@
 const hostname = window.location.hostname;
 
-console.log("===NODE_ENV===", process.env.NODE_ENV, "===HOSTNAME===", hostname);
-
 export const isLocal = hostname === "localhost" || hostname === "127.0.0.1";
 export const isStaging = hostname.startsWith("staging");
 export const isProduction = hostname === "yatra.iskconjiasarai.com";
 
 export const getApiBase = () => {
-  if(isStaging) return "https://23bqlz3il2.execute-api.ap-south-1.amazonaws.com/stg";
-  if(isProduction) return "https://lp0pdlvnca.execute-api.ap-south-1.amazonaws.com/prd";
+  if(isStaging && process.env.NODE_ENV === "stg") return "https://23bqlz3il2.execute-api.ap-south-1.amazonaws.com/stg";
+  if(isProduction && process.env.NODE_ENV === "prd") return "https://lp0pdlvnca.execute-api.ap-south-1.amazonaws.com/prd";
 
   return "http://localhost:3000";
 }
