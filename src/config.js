26b5eff85b7725728f39afb4f0536ea0df8c287e
aakashs007 <aakashs209@gmail.com>
2026-08-15 16:33:27 +0530

    fixup! add staging variable

--- src/config.js
+++ src/config.js
@@ -1,5 +1,16 @@
-const isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
+const hostname = window.location.hostname;
 
-export const API_BASE = isLocal
-  ? "https://23bqlz3il2.execute-api.ap-south-1.amazonaws.com/stg"
-  : "https://lp0pdlvnca.execute-api.ap-south-1.amazonaws.com/prd";
+console.log("===NODE_ENV===", process.env.NODE_ENV, "===HOSTNAME===", hostname);
+
+export const isLocal = hostname === "localhost" || hostname === "127.0.0.1";
+export const isStaging = hostname.startsWith("staging");
+export const isProduction = hostname === "yatra.iskconjiasarai.com";
+
+export const getApiBase = () => {
+  if(isStaging) return "https://23bqlz3il2.execute-api.ap-south-1.amazonaws.com/stg";
+  if(isProduction) return "https://lp0pdlvnca.execute-api.ap-south-1.amazonaws.com/prd";
+
+  return "http://localhost:3000";
+}
+
+export const API_BASE = getApiBase();
